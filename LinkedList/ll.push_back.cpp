d47c6dc616a50d1672053d3af3fa80106714fb48
mayanklohar <loharmayank75@gmail.com>
2026-01-17 23:08:14 +0530

    updated push_back code

--- LinkedList/ll.push_back.cpp
+++ LinkedList/ll.push_back.cpp
@@ -21,17 +21,7 @@ class List{
         tail = NULL;
     }
 
-    void push_front(int val){
-        Node* newNode = new Node(val);
-        if(head == NULL){
-            head = newNode;
-            tail = newNode;
-        }else{
-            newNode->next = head;
-            head = newNode;
-        }
-    }
-
+    
      void push_back(int val){
         Node* newNode = new Node(val);
         if(head == NULL){
@@ -55,11 +45,11 @@ class List{
 };
 int main(){
 List ll;
-ll.push_front(3);  
-ll.push_front(2);  
-ll.push_front(1);
-ll.printList();
+
 ll.push_back(4);
+ll.push_back(5);
+ll.push_back(6);
+ll.push_back(7);
 ll.printList();
 return 0;
 }
