f39a08762f85d44aac4b7e739b5b02d4b79ac43e
mayanklohar <loharmayank75@gmail.com>
2026-02-08 22:18:38 +0530

    Stack using vector and class template

---
+++ LinkedList/Stack/StackUsing_Template.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+template<class T>
+
+class Stack{
+    vector<T>vec;
+    public:
+    void push(T val){
+        vec.push_back(val);
+    }
+    void pop(){
+        if(isEmpty()){
+            cout<<"Stack is empty\n";
+            return;
+        }
+        vec.pop_back();
+    
+    }
+
+    T top(){
+        
+        int lastIdx=vec.size()-1;
+        return vec[lastIdx];
+    }
+
+    bool isEmpty(){
+        return vec.size()==0;   
+    }
+};
+
+
+    int main(){
+        Stack <char>s;
+        s.push('c');
+        s.push('b');
+        s.push('a');
+        while(!s.isEmpty()){
+            cout<<s.top()<<endl;
+            s.pop();
+        }
+        cout<<"Stack is now empty\n";
+       
+    }
