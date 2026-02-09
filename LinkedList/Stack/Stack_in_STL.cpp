8129cad6c61a06b5df760f213b290312601db8e2
mayanklohar <loharmayank75@gmail.com>
2026-02-09 22:15:12 +0530

    Stack in STL added

---
+++ LinkedList/Stack/Stack_in_STL.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    
+    cout<<"stack size: "<<s.size()<<endl; // 3
+    s.pop();
+    cout<<"element poped"<<endl;
+    cout<<s.top()<<endl; // 2
+    cout<<"stack size: "<<s.size()<<endl; // 2
+
+    return 0;
+}
