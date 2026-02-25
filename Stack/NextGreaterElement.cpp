639636813c69764188ad59f55697097397517d6d
mayanklohar <loharmayank75@gmail.com>
2026-02-25 22:42:26 +0530

    updated

--- Stack/NextGreaterElement.cpp
+++ Stack/NextGreaterElement.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void nextGreater(int arr[], int n) {
     stack<int> s;
 
+
     for(int i = n-1; i >= 0; i--) {
 
         while(!s.empty() && s.top() <= arr[i]) {
@@ -19,13 +20,13 @@ void nextGreater(int arr[], int n) {
         s.push(arr[i]);
     }
 }
-
+// Time Complexity: O(n)
 int main() {
     int arr[] = {4, 5, 2, 25};
     int n = 4;
 
     nextGreater(arr, n);
-  
+   
     
     return 0;
 }
