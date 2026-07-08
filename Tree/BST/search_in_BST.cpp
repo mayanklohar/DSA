d23d4086981dddb86732d876591c33c7e84d71a1
mayanklohar <loharmayank75@gmail.com>
2026-07-08 22:16:42 +0530

    search_in_BST

---
+++ Tree/BST/search_in_BST.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+     Node(int data){
+        this->data=data;
+        left=right=NULL;
+     }
+};
+
+Node* insert(Node* root , int val){
+    if(root==NULL){
+        root = new Node(val);
+        return root;    
+    }
+
+    if(val<root->data){
+        root->left=insert(root->left , val);
+    }
+    else{
+        root->right=insert(root->right , val);
+    }
+
+    return root;
+}
+
+Node* buildBST(int arr[] , int n){
+    Node* root=NULL;
+    for(int i=0;i<n;i++){
+        root=insert(root,arr[i]);
+    }
+    return root;
+}
+
+bool search(Node* root , int key){
+    if(root==NULL){
+        return false;
+    }
+
+    if(root->data==key){
+        return true;
+    }
+
+    if(key<root->data){
+        return search(root->left , key);
+    }
+    else{
+        return search(root->right , key);
+    }
+}
+
+int main(){
+    int arr[]={5,1,3,4,2,7};
+    Node* root=buildBST(arr,6);
+    cout<<search(root,4);
+    cout<<endl;
+    return 0;
+}
