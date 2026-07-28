97e04d9dbccca71c902e9ffc80b9a4b5a3e614d8
mayanklohar <loharmayank75@gmail.com>
2026-07-28 23:44:35 +0530

    used Sliding Window for finding maximum of subarrays of size K

---
+++ Heaps/Sliding_window_maximum.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+void slidingWindowMax(vector<int>arr , int K){
+    priority_queue<pair<int,int>>pq;
+
+    //1st window
+    for(int i=0 ; i<K ; i++){
+        pq.push(make_pair(arr[i],i));
+    }
+
+    cout<<"output "<<pq.top().first<<" ";
+
+    for(int i=K ; i<arr.size() ; i++){
+        while(!pq.empty() && pq.top().second<=(i-K)){
+            pq.pop();
+        }
+
+        pq.push(make_pair(arr[i],i));
+        cout<<pq.top().first<<" ";
+    }
+    cout<<endl;
+    }
+
+    int main(){
+        vector<int> arr = {
+    1,3,-1,-3,5,3,6,7
+        } ;
+
+        int K=3;
+    slidingWindowMax(arr,K);
+
+return 0;
+}
