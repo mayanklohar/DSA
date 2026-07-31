68b90f7996c34f17d613f93421a79c4f3ea78716
mayanklohar <loharmayank75@gmail.com>
2026-07-31 21:59:22 +0530

    written insert function for hash table

--- Hashing/Building_hash_table.cpp
+++ Hashing/Building_hash_table.cpp
@@ -22,6 +22,15 @@ class HashTable{
     int currSize;
     Node** table;
 
+    int hashFunction(string key){
+        int idx=0;
+
+        for(int i=0 ; i<key.size() ; i++){
+            idx=idx+(key[i] * key[i] % totSize);
+        }
+        return idx;
+    }
+
     HashTable(int size=5){
         totSize=size;
         currSize=0;
@@ -31,6 +40,16 @@ class HashTable{
         for(int i=0 ; i<totSize ; i++){
             table[i]=NULL;
         }
+    }
+
+    void insert(string key , int val){
+        int idx = hashFunction(key);
+
+        Node* newNode=new Node(key,val);
+        Node* head=table[idx];
+
+        newNode->next=head;
+        head=newNode;
     }
 };
 
